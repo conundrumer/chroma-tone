c679aa4e2e127b7eeb1d9cbf8c8c1fb3fcd6811a
David Lu <davidelu93@gmail.com>
2015-05-20 14:25:04 -0400

    change testing to random sampling

--- float-test/server.js
+++ float-test/server.js
@@ -9,7 +9,6 @@ var PORT = consts.PORT;
 var sqrt = Math.sqrt;
 
 var NUM_TESTS_PER_CHUNK = consts.NUM_TESTS_PER_CHUNK;
-var INTERVAL = 1000;
 var MAX_NUM = Long.fromString(float.INF_HEX, false, 16);
 
 // Create a server instance, and chain the listen function to it
@@ -17,6 +16,8 @@ var MAX_NUM = Long.fromString(float.INF_HEX, false, 16);
 // The sock object the callback function receives UNIQUE for each connection
 net.createServer((sock) => {
     var timeout;
+    var m = Long.fromString(float.ZERO_HEX, 16);
+    var tests = [];
 
     // We have a connection - a socket object is assigned to the connection automatically
     console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
@@ -24,10 +25,20 @@ net.createServer((sock) => {
     // Add a 'data' event handler to this instance of socket
     sock.on('data', (data) => {
 
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        // Write the data back to the socket, the client will receive it as data from the server
-        sock.write('You said "' + data + '"');
+        // console.log('DATA ' + sock.remoteAddress + ': ' + data);
 
+          if (data.toString() === 'received' && m.lessThanOrEqual(MAX_NUM)) {
+            if (m.modulo(0x10000).toNumber() === 0) {
+                console.log(m.toString());
+            }
+            m = m.add(NUM_TESTS_PER_CHUNK);
+            sendTests();
+            generateTests(m);
+          } else {
+              console.log('failed tests', tests.map((c, i) => c + ' ' + tests[i+1])
+                .filter((c, i) => (i % 2) === 0)
+                .join('\n'));
+          }
     });
 
     // Add a 'close' event handler to this instance of socket
@@ -40,27 +51,27 @@ net.createServer((sock) => {
     // n is long
     // sends an array of hex strings eg [x, sqrt(x), x+1, sqrt(x+1)...] to client
     function generateTests(n) {
-      let tests = [];
+        tests = [];
       for (let i = 0; i < NUM_TESTS_PER_CHUNK; i++) {
         if (n.add(i).greaterThan(MAX_NUM)) {
             return;
         }
 
-        let h = n.add(i).toString(16);
+        /* random sampling instead of exhaustive testing */
+        // let h = n.add(i).toString(16);
+        let h = float.random();
         let f = float.fromHex(h);
 
         tests.push(h, float.toHex(sqrt(f)));
       }
-
-      sock.write(JSON.stringify(tests));
-
-      // console.log(tests);
-      if (n.lessThanOrEqual(MAX_NUM)) {
-        timeout = setTimeout(generateTests.bind(null, n.add(NUM_TESTS_PER_CHUNK)), INTERVAL);
-      }
     }
 
-    generateTests(Long.UZERO);
+    function sendTests() {
+      sock.write(tests.join(' '));
+    }
+
+    generateTests(m);
+    sendTests();
 
 }).listen(PORT, HOST);
 
