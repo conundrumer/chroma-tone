356e85bbc9a134af5a9a780713e96a08b9f99ea0
David Lu <davidelu93@gmail.com>
2015-05-22 19:47:17 -0400

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var React = require('react/addons');
+var ReactTransitionGroup = React.addons.TransitionGroup;
+
+// CSS
+require('normalize.css');
+require('../styles/main.css');
+
+var imageURL = require('../images/yeoman.png');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className='main'>
+        <ReactTransitionGroup transitionName="fade">
+          <img src={imageURL} />
+        </ReactTransitionGroup>
+      </div>
+    );
+  }
+});
+React.render(<App />, document.getElementById('content'));
+
+module.exports = App;
