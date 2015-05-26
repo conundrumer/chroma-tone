f5b48803f5f55502a362f8d85cbdf794c33a0d43
David Lu <davidelu93@gmail.com>
2015-05-26 02:28:54 -0400

    add buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,22 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: pkgConfig,
 
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:conundrumer/lr-editor.git',
+          branch: 'gh-pages'
+        }
+      }
+    },
+
     webpack: {
       options: webpackDistConfig,
       dist: {
