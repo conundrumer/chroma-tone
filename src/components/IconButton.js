08b454366aa8e15ed9c53f3946ae629c75326e50
David Lu <davidelu93@gmail.com>
2015-06-11 04:39:29 -0400

    seriously hack in upwards tooltip + fix hotkey that uses mod

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,6 +1,8 @@
+/* eslint no-underscore-dangle: 0*/
 'use strict';
 
 var React = require('react/addons');
+var Transitions = require('material-ui/lib/styles/transitions');
 
 var MuiIconButton = require('material-ui').IconButton;
 var FontIcon = require('material-ui').FontIcon;
@@ -8,6 +10,12 @@ var FontIcon = require('material-ui').FontIcon;
 var {Line, Curve, MultiLine, Viewfinder, CursorMove, OnionSkin} = require('./SvgIcons');
 
 var IconButton = React.createClass({
+
+  getDefaultProps() {
+    return {
+      style: {}
+    };
+  },
 
   getInitialState() {
     return {
@@ -19,12 +27,26 @@ var IconButton = React.createClass({
     this.bindHotkey(this.props.hotkey);
   },
 
+  componentDidMount() {
+    // lol
+    this.oldGetIconButtonStyles = this.refs.iconButton.getStyles;
+    this.refs.iconButton.getStyles = this.getIconButtonStyles;
+    // if (this.props.tooltip) {
+      this.oldGetTooltipStyles = this.refs.iconButton.refs.tooltip.getStyles;
+      this.refs.iconButton.refs.tooltip.getStyles = this.getTooltipStyles;
+    // }
+    this._positionTooltip();
+  },
+
   shouldComponentUpdate(nextProps, nextState) {
     return this.props.icon !== nextProps.icon ||
       this.props.hotkey !== nextProps.hotkey ||
       this.state.keyPressed !== nextState.keyPressed ||
       this.props.selected !== nextProps.selected ||
-      !!this.props.style && !!nextProps.style && this.props.style.transform !== nextProps.style.transform;
+      this.props.tooltip !== nextProps.tooltip ||
+      !this.props.showTooltip && nextProps.showTooltip ||
+      !!this.props.iconStyle && !!nextProps.iconStyle &&
+        this.props.iconStyle.transform !== nextProps.iconStyle.transform;
   },
 
   componentWillUpdate(nextProps, nextState) {
@@ -41,6 +63,10 @@ var IconButton = React.createClass({
       this.endRipple();
       this.props.onTouchTap();
     }
+
+    if (this.props.tooltip !== nextProps.tooltip) {
+      this._positionTooltip();
+    }
   },
 
   componentWillUnmount() {
@@ -49,6 +75,46 @@ var IconButton = React.createClass({
     // if (this.unbindHotkey) {
     //   this.unbindHotkey();
     // }
+  },
+
+  getIconButtonStyles() {
+    var styles = this.oldGetIconButtonStyles();
+    var margin = (this.props.style.width || 48) + 4;
+    styles.tooltip.marginTop = margin;
+    if (this.props.upwardsTooltip) {
+      styles.tooltip.marginTop = 0;
+      styles.tooltip.marginBottom = margin;
+    }
+    if (!this.props.showTooltip) {
+      styles.tooltip.display = 'none';
+    }
+    return styles;
+  },
+
+  getTooltipStyles() {
+    var styles = this.oldGetTooltipStyles();
+    if (this.props.upwardsTooltip) {
+      styles.root.bottom = styles.root.top;
+      styles.root.top = '';
+      styles.root.transition += ',' + Transitions.easeOut('0ms', 'bottom', '450ms');
+      styles.ripple.bottom = styles.ripple.top;
+      styles.ripple.top = '';
+      styles.ripple.transform = 'translate(-50%, 50%)';
+      styles.rootWhenShown.bottom = styles.rootWhenShown.top;
+      styles.rootWhenShown.top = '';
+      styles.rootWhenShown.transform = 'translate3d(0px, -16px, 0px)';
+      styles.rootWhenShown.transition += ',' + Transitions.easeOut('0ms', 'bottom', '0ms');
+    }
+    return styles;
+  },
+
+  _positionTooltip: function() {
+    var tooltip = React.findDOMNode(this.refs.iconButton.refs.tooltip);
+    var tooltipWidth = tooltip.offsetWidth;
+    var buttonWidth = this.props.style.width || 48;
+
+    tooltip.style.left = (tooltipWidth - buttonWidth) / 2 * -1 + 'px';
+    this.setState({});
   },
 
   bindHotkey(hotkey) {
@@ -65,16 +131,22 @@ var IconButton = React.createClass({
     // };
   },
 
-  onHotkeyDown() {
+  onHotkeyDown(e) {
     if (this.isMounted()) {
-      this.setState({ keyPressed: true });
+      if (this.props.hotkey.includes('mod')) {
+        this.setState({ keyPressed: true }, () => this.setState({ keyPressed: false }));
+      } else {
+        this.setState({ keyPressed: true });
+      }
     }
+    return false;
   },
 
   onHotkeyUp() {
     if (this.isMounted()) {
       this.setState({ keyPressed: false });
     }
+    return false;
   },
 
   // lol
@@ -105,22 +177,18 @@ var IconButton = React.createClass({
   },
 
   render() {
-    var style = this.props.style || {};
+    var style = this.props.style;
     style.background = 'null';
     style.transition = '0';
-    var className = 'icon-button' + (this.props.selected ? ' selected blue' : '');
+    var className = 'icon-button' + (this.props.selected ? ' selected ' + (this.props.selectedColor || 'blue') : '');
     var icon = this.getIcon(this.props.icon);
-    // var selectRing = <div className={'select-ring' + (this.props.selected ? ' selected' : '')} />;
-    if (icon) {
-      return (
-        <MuiIconButton className={className} ref='iconButton' {...this.props} style={style}>
-          {icon}
-        </MuiIconButton>
-      );
-    }
+
     return (
       <MuiIconButton className={className} ref='iconButton' {...this.props} style={style}>
-        <FontIcon className={'mdi mdi-' + this.props.icon} />
+        {
+          icon ? icon :
+          <FontIcon className={'mdi mdi-' + this.props.icon} style={this.props.iconStyle} />
+        }
       </MuiIconButton>
     );
   }
