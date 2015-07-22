c8fe48461d663605ca9278f67e29ba862e296a8e
David Lu <davidelu93@gmail.com>
2015-07-22 17:33:52 -0400

    Update grid.js to use updated lines + replace getCellkey with getCellHash + optimize getCellsPosFromLine

--- grid.js
+++ grid.js
@@ -4,8 +4,12 @@ var _ = require('lodash');
 
 var { getBox, inBounds } = require('./geo-utils');
 
-function getCellKey(cellPos) {
-  return cellPos.x + '_' + cellPos.y;
+// http://stackoverflow.com/a/13871379
+function getCellHash(a, b) {
+  var A = (a >= 0 ? 2 * a : -2 * a - 1);
+  var B = (b >= 0 ? 2 * b : -2 * b - 1);
+  var C = ((A >= B ? A * A + A + B : A + B * B) / 2);
+  return a < 0 && b < 0 || a >= 0 && b >= 0 ? C : -C - 1;
 }
 
 // cell containing unordered unique lines
@@ -15,10 +19,7 @@ class Cell {
     this.x = cellPos.x;
     this.y = cellPos.y;
     this.lines = Object.create(null);
-  }
-
-  get key() {
-    return getCellKey(this);
+    this.key = getCellHash(cellPos.x, cellPos.y);
   }
 
   has(line) {
@@ -45,7 +46,6 @@ class OrderedCell extends Cell{
     super(cellPos);
     this.lines = [];
   }
-
 
   getIndexOf(line) {
     return _.sortedIndex(this.lines, line, l => -l.id);
@@ -74,6 +74,10 @@ class OrderedCell extends Cell{
   }
 }
 
+function cellsEqual(cell1, cell2) {
+  return cell1.x === cell2.x && cell1.y === cell2.y;
+}
+
 class Grid {
 
   constructor(gridSize, isOrdered) {
@@ -83,22 +87,28 @@ class Grid {
     this.linesToCells = Object.create(null);
   }
 
-  getLinesFromCell(cellPos) {
-    let cell = this.cells[getCellKey(cellPos)];
-    if (cell === undefined) {
-      return [];
-    }
-    return cell.getLines();
+  getCell(k) {
+    return this.cells[k];
+  }
+
+  getLinesFromCell(cellX, cellY) {
+    let key = getCellHash(cellX, cellY);
+    let cell = this.getCell(key);
+
+    return cell ? cell.getLines() : [];
   }
 
   addLine(line) {
     // array of cell positions
     let cellsPos = this.getCellsPosFromLine(line);
-    cellsPos.forEach( cellPos => this.addLineToCell(line, cellPos) );
+    // _.forEach(cellsPos, cellPos => this.addLineToCell(line, cellPos) );
+    for (let i = 0; i < cellsPos.length; i++) {
+      this.addLineToCell(line, cellsPos[i]);
+    }
   }
 
   addLineToCell(line, cellPos) {
-    let cellKey = getCellKey(cellPos);
+    let cellKey = getCellHash(cellPos.x, cellPos.y);
 
     if (this.cells[cellKey] === undefined) {
       this.cells[cellKey] = this.isOrdered ? new OrderedCell(cellPos) : new Cell(cellPos);
@@ -135,11 +145,11 @@ class Grid {
 
   // digital differential analyzer
   getCellsPosFromLine(line) {
-    let { x1, y1, x2, y2 } = line;
-    if ( x1 === x2 && y1 === y2 || _.eq(this.getCellPos(x1, y1), this.getCellPos(x2, y2))) {
+    let { x1, x2, y1, y2 } = line;
+    if ( x1 === x2 && y1 === y2 || cellsEqual(this.getCellPos(x1, y1), this.getCellPos(x2, y2))) {
       return [ this.getCellPos(x1, y1) ];
     }
-    let flip = (Math.abs(line.dy) > Math.abs(line.dx));
+    let flip = (Math.abs(line.vec.y) > Math.abs(line.vec.x));
     if (flip) { // handle steep lines by temporarily flipping the axes
       [ x1, y1, x2, y2 ] = [ y1, x1, y2, x2 ];
     }
@@ -151,22 +161,36 @@ class Grid {
     let slope = (y2 - y1) / (x2 - x1);
     let getY = (x) => y1 + slope * (x - x1);
 
-    return _(_.range(cellPosStart.x + 1, cellPosEnd.x + 1)) // grid lines not grid cells
-      .map( cellX => { // get initial cellsPos
-        return { x: cellX, y: Math.floor(getY(cellX * this.gridSize) / this.gridSize) };
-      })
-      .unshift(cellPosStart).push(cellPosEnd) // add first cell and last cell
-      .map( (cellPos, i, cellsPos) => // add cells missing from vertical crossings
-        (i > 0 && cellsPos[i-1].x !== cellPos.x && cellsPos[i-1].y !== cellPos.y)
-          ? [{ x: cellPos.x-1, y: cellPos.y }, cellPos]
-          : [cellPos]
-      ).flatten()
-      .thru( cellsPos => // remove duplicate last cell
-        _.eq(_.last(cellsPos), cellsPos[cellsPos.length-2])
-          ? _.initial(cellsPos)
-          : cellsPos
-      ).map( ({x, y}) => flip ? { x: y, y: x } : {x, y} )
-      .value();
+    let cellsPos = [ cellPosStart ];
+    let prevCell = cellsPos[0];
+    let addNextCell = (nextCell) => {
+      if (prevCell.x !== nextCell.x && prevCell.y !== nextCell.y) {
+        cellsPos.push({ x: nextCell.x-1, y: nextCell.y });
+      }
+      cellsPos.push(nextCell);
+      prevCell = nextCell;
+    };
+
+    for (let cx = cellPosStart.x + 1; cx < cellPosEnd.x + 1; cx++) {
+      let nextCell = { x: cx, y: Math.floor(getY(cx * this.gridSize) / this.gridSize) };
+      addNextCell(nextCell);
+    }
+    addNextCell(cellPosEnd);
+
+    if (cellsEqual(cellsPos[cellsPos.length-1], cellsPos[cellsPos.length-2])) {
+      cellsPos.pop();
+    }
+
+    if (flip) {
+      for (let i = 0; i < cellsPos.length; i++) {
+        let cell = cellsPos[i];
+        let { x, y } = cell;
+        cell.x = y;
+        cell.y = x;
+      }
+    }
+
+    return cellsPos;
   }
 
 }
@@ -186,11 +210,11 @@ class GridV62 extends Grid {
   getCellsPosFromLine(line) {
     var cellsPos = [];
 
-    let cellPosStart = this.getCellPosAndOffset(line.x1, line.y1);
-    let cellPosEnd = this.getCellPosAndOffset(line.x2, line.y2);
+    let cellPosStart = this.getCellPosAndOffset(line.p.x, line.p.y);
+    let cellPosEnd = this.getCellPosAndOffset(line.q.x, line.q.y);
 
     cellsPos.push(cellPosStart);
-    if (line.dx === 0 && line.dy === 0 || cellPosStart.x === cellPosEnd.x && cellPosStart.y === cellPosEnd.y) {
+    if (line.vec.x === 0 && line.vec.y === 0 || cellPosStart.x === cellPosEnd.x && cellPosStart.y === cellPosEnd.y) {
       return cellsPos; // done
     }
 
@@ -198,10 +222,10 @@ class GridV62 extends Grid {
 
     let getNextPos;
 
-    if (line.dx === 0) {
+    if (line.vec.x === 0) {
       getNextPos = (l, x, y, dx, dy) => { return { x: x, y: y + dy }; };
 
-    } else if (line.dy === 0) {
+    } else if (line.vec.y === 0) {
       getNextPos = (l, x, y, dx, dy) => { return { x: x + dx, y: y }; };
 
     } else {
@@ -220,7 +244,7 @@ class GridV62 extends Grid {
       }
     };
 
-    addNextCell(cellPosStart, { x: line.x1, y: line.y1 });
+    addNextCell(cellPosStart, { x: line.p.x, y: line.p.y });
 
     return cellsPos;
   }
@@ -228,20 +252,20 @@ class GridV62 extends Grid {
   getDelta(line, cellPos) {
     let dx, dy;
     if (cellPos.x < 0) {
-      dx = (this.gridSize + cellPos.gx) * (line.dx > 0 ? 1 : -1);
+      dx = (this.gridSize + cellPos.gx) * (line.vec.x > 0 ? 1 : -1);
     } else {
-      dx = -cellPos.gx + (line.dx > 0 ? this.gridSize : -1);
+      dx = -cellPos.gx + (line.vec.x > 0 ? this.gridSize : -1);
     }
     if (cellPos.y < 0) {
-      dy = (this.gridSize + cellPos.gy) * (line.dy > 0 ? 1 : -1);
+      dy = (this.gridSize + cellPos.gy) * (line.vec.y > 0 ? 1 : -1);
     } else {
-      dy = -cellPos.gy + (line.dy > 0 ? this.gridSize : -1);
+      dy = -cellPos.gy + (line.vec.y > 0 ? this.gridSize : -1);
     }
     return { x: dx, y: dy };
   }
 
   getNextPos(line, x, y, dx, dy) {
-    let slope = line.dy / line.dx;
+    let slope = line.vec.y / line.vec.x;
     let yNext = y + slope * dx;
     if (Math.abs(yNext - y) < Math.abs(dy)) {
       return {
@@ -256,7 +280,7 @@ class GridV62 extends Grid {
       };
     }
     return {
-      x: x + line.dx * dy / line.dy,
+      x: x + line.vec.x * dy / line.vec.y,
       y: y + dy
     };
   }
@@ -266,14 +290,14 @@ class GridV61 extends GridV62 {
 
   getDelta(line, cellPos) {
     return {
-      x: -cellPos.gx + (line.dx > 0 ? this.gridSize : -1),
-      y: -cellPos.gy + (line.dy > 0 ? this.gridSize : -1)
+      x: -cellPos.gx + (line.vec.x > 0 ? this.gridSize : -1),
+      y: -cellPos.gy + (line.vec.y > 0 ? this.gridSize : -1)
     };
   }
 
   getNextPos(line, x, y, dx, dy) {
-    let slope = line.dy / line.dx;
-    let yIsThisBelowActualY0 = line.y1 - slope * line.x1;
+    let slope = line.vec.y / line.vec.x;
+    let yIsThisBelowActualY0 = line.p.y - slope * line.p.x;
     let yDoesThisEvenWork = Math.round(slope * (x + dx) + yIsThisBelowActualY0);
     if (Math.abs(yDoesThisEvenWork - y) < Math.abs(dy)) {
       return {
@@ -295,4 +319,4 @@ class GridV61 extends GridV62 {
 
 }
 
-module.exports = { Grid, GridV62, GridV61 };
+module.exports = { Grid, GridV62, GridV61, getCellHash };
