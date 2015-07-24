e3250179a561ed238a74b8e7307ae9735f2fb88c
David Lu <davidelu93@gmail.com>
2015-07-24 10:14:07 -0400

    extract riderBody.js out of rider.js

--- src/rider.js
+++ src/rider.js
@@ -1,93 +1,24 @@
-/*eslint no-multi-spaces: 0 comma-spacing: 0*/
 'use strict';
 
 var _ = require('lodash');
 
+var { Entity } = require('./entities');
+
 var {
-  Entity,
-  Point,
-  Stick,
-  BindStick,
-  RepelStick,
-  ScarfStick,
-  ClockwiseCrashJoint
-} = require('./entities');
+  makeRider,
+  copyRider,
+  getBodyParts,
+  STRING_PEG_TAIL
+} = require('./riderBody');
 
 const
-  // enum
-  STICK = 0,
-  BIND_STICK = 1,
-  REPEL_STICK = 2,
   // physics
   ITERATE = 6,
   GRAVITY = { x: 0, y: 0.175 },
-  ENDURANCE = 0.057,
   VX_INIT = 0.4,
   VY_INIT = 0,
   SCARF_FLUTTER_INTENSITY = 0.2,
-  SPEED_THRESHOLD_FLUTTER = 125, // as this gets smaller, the scarf intensifies faster while speed increases
-  // points
-  PEG      = { id: 0 , xInit: 0    , yInit: 0    , friction: 0.8 },
-  TAIL     = { id: 1 , xInit: 0    , yInit: 5    , friction: 0   },
-  NOSE     = { id: 2 , xInit: 15   , yInit: 5    , friction: 0   },
-  STRING   = { id: 3 , xInit: 17.5 , yInit: 0    , friction: 0   },
-  BUTT     = { id: 4 , xInit: 5    , yInit: 0    , friction: 0.8 },
-  SHOULDER = { id: 5 , xInit: 5    , yInit: -5.5 , friction: 0.8 },
-  LHAND    = { id: 6 , xInit: 11.5 , yInit: -5   , friction: 0.1 },
-  RHAND    = { id: 7 , xInit: 11.5 , yInit: -5   , friction: 0.1 },
-  LFOOT    = { id: 8 , xInit: 10   , yInit: 5    , friction: 0   },
-  RFOOT    = { id: 9 , xInit: 10   , yInit: 5    , friction: 0   },
-  POINTS = [ PEG, TAIL, NOSE, STRING, BUTT, SHOULDER, LHAND, RHAND, LFOOT, RFOOT ],
-  // constraints
-  PEG_TAIL         = { id: 0  , p: PEG      , q: TAIL   , type: STICK       },
-  TAIL_NOSE        = { id: 1  , p: TAIL     , q: NOSE   , type: STICK       },
-  NOSE_STRING      = { id: 2  , p: NOSE     , q: STRING , type: STICK       },
-  STRING_PEG       = { id: 3  , p: STRING   , q: PEG    , type: STICK       },
-  PEG_NOSE         = { id: 4  , p: PEG      , q: NOSE   , type: STICK       },
-  STRING_TAIL      = { id: 5  , p: STRING   , q: TAIL   , type: STICK       },
-  PEG_BUTT         = { id: 6  , p: PEG      , q: BUTT   , type: BIND_STICK  },
-  TAIL_BUTT        = { id: 7  , p: TAIL     , q: BUTT   , type: BIND_STICK  },
-  NOSE_BUTT        = { id: 8  , p: NOSE     , q: BUTT   , type: BIND_STICK  },
-  SHOULDER_BUTT    = { id: 9  , p: SHOULDER , q: BUTT   , type: STICK       },
-  SHOULDER_LHAND   = { id: 10 , p: SHOULDER , q: LHAND  , type: STICK       },
-  SHOULDER_RHAND   = { id: 11 , p: SHOULDER , q: RHAND  , type: STICK       },
-  BUTT_LFOOT       = { id: 12 , p: BUTT     , q: LFOOT  , type: STICK       },
-  BUTT_RFOOT       = { id: 13 , p: BUTT     , q: RFOOT  , type: STICK       },
-  SHOULDER_RHAND_2 = { id: 14 , p: SHOULDER , q: RHAND  , type: STICK       }, // DUPLICATE!
-  SHOULDER_PEG     = { id: 15 , p: SHOULDER , q: PEG    , type: BIND_STICK  },
-  STRING_LHAND     = { id: 16 , p: STRING   , q: LHAND  , type: BIND_STICK  },
-  STRING_RHAND     = { id: 17 , p: STRING   , q: RHAND  , type: BIND_STICK  },
-  LFOOT_NOSE       = { id: 18 , p: LFOOT    , q: NOSE   , type: BIND_STICK  },
-  RFOOT_NOSE       = { id: 19 , p: RFOOT    , q: NOSE   , type: BIND_STICK  },
-  SHOULDER_LFOOT   = { id: 20 , p: SHOULDER , q: LFOOT  , type: REPEL_STICK },
-  SHOULDER_RFOOT   = { id: 21 , p: SHOULDER , q: RFOOT  , type: REPEL_STICK },
-  CONSTRAINTS = [
-    PEG_TAIL, TAIL_NOSE, NOSE_STRING, STRING_PEG, PEG_NOSE, STRING_TAIL,
-    PEG_BUTT, TAIL_BUTT, NOSE_BUTT,
-    SHOULDER_BUTT, SHOULDER_LHAND, SHOULDER_RHAND, BUTT_LFOOT, BUTT_RFOOT, SHOULDER_RHAND_2,
-    SHOULDER_PEG, STRING_LHAND, STRING_RHAND, LFOOT_NOSE, RFOOT_NOSE,
-    SHOULDER_LFOOT, SHOULDER_RFOOT
-  ],
-  // joints
-  STRING_PEG_TAIL = { id: 0 , s: PEG_TAIL      , t: STRING_PEG },
-  BODY_SLED =       { id: 1 , s: SHOULDER_BUTT , t: STRING_PEG },
-  JOINTS = [ STRING_PEG_TAIL, BODY_SLED ],
-  // body parts
-  BODY_PARTS = [
-    { name: 'sled'     , p: PEG      , q: STRING   },
-    { name: 'body'     , p: BUTT     , q: SHOULDER },
-    { name: 'leftArm'  , p: SHOULDER , q: LHAND    },
-    { name: 'rightArm' , p: SHOULDER , q: RHAND    },
-    { name: 'leftLeg'  , p: BUTT     , q: LFOOT    },
-    { name: 'rightLeg' , p: BUTT     , q: RFOOT    }
-  ],
-  // scarf
-  SCARF = {
-    airFriction: 0.85,
-    p: SHOULDER,
-    numSegments: 7,
-    segmentLength: 2
-  };
+  SPEED_THRESHOLD_FLUTTER = 125; // as this gets smaller, the scarf intensifies faster while speed increases
 
 
 class Rider extends Entity {
@@ -96,59 +27,11 @@ class Rider extends Entity {
     this.crashed = false;
     this.sledBroken = false;
 
-    // TODO: separate rider state creation/management and physics
-    this.makePoints();
-    this.makeConstraints();
-    this.makeScarf();
-    this.makeJoints();
+    makeRider(this);
 
     this.initPosAndVel(x, y, vx, vy);
   }
 
-  makePoints() {
-    this.points = POINTS.map( p => {
-      let point = new Point(p.id, p.xInit, p.yInit, p.friction);
-      return point;
-    });
-  }
-  makeConstraints() {
-    this.constraints = CONSTRAINTS.map( c => {
-      let stick;
-      let p = this.points[c.p.id];
-      let q = this.points[c.q.id];
-      switch(c.type) {
-        case STICK:
-          stick = new Stick(c.id, p, q);
-          break;
-        case BIND_STICK:
-          stick = new BindStick(c.id, p, q, ENDURANCE);
-          break;
-        case REPEL_STICK:
-          stick = new RepelStick(c.id, p, q);
-          stick.restLength *= 0.5;
-          break;
-        default:
-          throw new Error('Unknown constraint type');
-      }
-      return stick;
-    });
-  }
-  makeScarf() {
-    this.scarfPoints = [];
-    this.scarfConstraints = [];
-
-    for (let i = 0; i < SCARF.numSegments; i++) {
-      let p = (i === 0) ? this.points[SCARF.p.id] : this.scarfPoints[i-1];
-      let q = new Point(-i - 1, p.x - SCARF.segmentLength, p.y, 0, SCARF.airFriction);
-      this.scarfPoints.push(q);
-      this.scarfConstraints.push(new ScarfStick(-i - 1, p, q));
-    }
-  }
-  makeJoints() {
-    this.joints = JOINTS.map( j =>
-      new ClockwiseCrashJoint(j.id, this.constraints[j.s.id], this.constraints[j.t.id])
-    );
-  }
   initPosAndVel(x, y, vx, vy) {
     this.points.concat(this.scarfPoints).forEach(p => {
       p.pos.add({ x: x, y: y });
@@ -225,28 +108,7 @@ class Rider extends Entity {
   }
 
   copyState() {
-    let copy = {};
-    copy.points = _.map(this.points, point => point.clone());
-    copy.constraints = _.map(this.constraints, (c, i) => {
-      let cProps = CONSTRAINTS[i];
-      return c.clone(copy.points[cProps.p.id], copy.points[cProps.q.id]);
-    });
-
-    copy.scarfPoints = _.map(this.scarfPoints, point => point.clone());
-    copy.scarfConstraints = _.map(this.scarfConstraints, (c, i) => {
-      let p = (i === 0) ? copy.points[SCARF.p.id] : copy.scarfPoints[i-1];
-      let q = copy.scarfPoints[i];
-      return c.clone(p, q);
-    });
-
-    copy.joints = _.map(this.joints, (j, i) => {
-      let jProps = JOINTS[i];
-      j.clone(copy.constraints[jProps.s.id], copy.constraints[jProps.t.id]);
-    });
-
-    copy.crashed = this.crashed;
-    copy.sledBroken = this.sledBroken;
-    return copy;
+    return copyRider(this);
   }
 
   getState() {
@@ -268,34 +130,8 @@ class Rider extends Entity {
     });
   }
 
-  static getBodyParts(state) {
-    let getPosition = (p, q) => {
-      let vec = q.pos.clone().subtract(p.pos);
-      return {
-        x: p.x,
-        y: p.y,
-        angle: Math.atan2(vec.y, vec.x)
-      };
-    };
-
-    let bodyParts = {};
-    BODY_PARTS.forEach(bodyPart => {
-      let p = state.points[bodyPart.p.id];
-      let q = state.points[bodyPart.q.id];
-      bodyParts[bodyPart.name] = getPosition(p, q);
-    });
-
-    bodyParts.scarf = [];
-    for (let i = 0; i < state.scarfConstraints.length; i++) {
-      let p = (i === 0) ? state.points[SCARF.p.id] : state.scarfPoints[i-1];
-      let q = state.scarfPoints[i];
-      bodyParts.scarf.push(getPosition(p, q));
-    }
-    return bodyParts;
-  }
-
   getBodyParts() {
-    return Rider.getBodyParts(this);
+    return getBodyParts(this);
   }
 
 }
---
+++ src/riderBody.js
@@ -0,0 +1,196 @@
+/*eslint no-multi-spaces: 0 comma-spacing: 0*/
+
+var _ = require('lodash');
+
+var {
+  Point,
+  Stick,
+  BindStick,
+  RepelStick,
+  ScarfStick,
+  ClockwiseCrashJoint
+} = require('./entities');
+
+const
+  ENDURANCE = 0.057,
+  // enum
+  STICK = 0,
+  BIND_STICK = 1,
+  REPEL_STICK = 2,
+  // points
+  PEG      = { id: 0 , xInit: 0    , yInit: 0    , friction: 0.8 },
+  TAIL     = { id: 1 , xInit: 0    , yInit: 5    , friction: 0   },
+  NOSE     = { id: 2 , xInit: 15   , yInit: 5    , friction: 0   },
+  STRING   = { id: 3 , xInit: 17.5 , yInit: 0    , friction: 0   },
+  BUTT     = { id: 4 , xInit: 5    , yInit: 0    , friction: 0.8 },
+  SHOULDER = { id: 5 , xInit: 5    , yInit: -5.5 , friction: 0.8 },
+  LHAND    = { id: 6 , xInit: 11.5 , yInit: -5   , friction: 0.1 },
+  RHAND    = { id: 7 , xInit: 11.5 , yInit: -5   , friction: 0.1 },
+  LFOOT    = { id: 8 , xInit: 10   , yInit: 5    , friction: 0   },
+  RFOOT    = { id: 9 , xInit: 10   , yInit: 5    , friction: 0   },
+  POINTS = [ PEG, TAIL, NOSE, STRING, BUTT, SHOULDER, LHAND, RHAND, LFOOT, RFOOT ],
+  // constraints
+  PEG_TAIL         = { id: 0  , p: PEG      , q: TAIL   , type: STICK       },
+  TAIL_NOSE        = { id: 1  , p: TAIL     , q: NOSE   , type: STICK       },
+  NOSE_STRING      = { id: 2  , p: NOSE     , q: STRING , type: STICK       },
+  STRING_PEG       = { id: 3  , p: STRING   , q: PEG    , type: STICK       },
+  PEG_NOSE         = { id: 4  , p: PEG      , q: NOSE   , type: STICK       },
+  STRING_TAIL      = { id: 5  , p: STRING   , q: TAIL   , type: STICK       },
+  PEG_BUTT         = { id: 6  , p: PEG      , q: BUTT   , type: BIND_STICK  },
+  TAIL_BUTT        = { id: 7  , p: TAIL     , q: BUTT   , type: BIND_STICK  },
+  NOSE_BUTT        = { id: 8  , p: NOSE     , q: BUTT   , type: BIND_STICK  },
+  SHOULDER_BUTT    = { id: 9  , p: SHOULDER , q: BUTT   , type: STICK       },
+  SHOULDER_LHAND   = { id: 10 , p: SHOULDER , q: LHAND  , type: STICK       },
+  SHOULDER_RHAND   = { id: 11 , p: SHOULDER , q: RHAND  , type: STICK       },
+  BUTT_LFOOT       = { id: 12 , p: BUTT     , q: LFOOT  , type: STICK       },
+  BUTT_RFOOT       = { id: 13 , p: BUTT     , q: RFOOT  , type: STICK       },
+  SHOULDER_RHAND_2 = { id: 14 , p: SHOULDER , q: RHAND  , type: STICK       }, // DUPLICATE!
+  SHOULDER_PEG     = { id: 15 , p: SHOULDER , q: PEG    , type: BIND_STICK  },
+  STRING_LHAND     = { id: 16 , p: STRING   , q: LHAND  , type: BIND_STICK  },
+  STRING_RHAND     = { id: 17 , p: STRING   , q: RHAND  , type: BIND_STICK  },
+  LFOOT_NOSE       = { id: 18 , p: LFOOT    , q: NOSE   , type: BIND_STICK  },
+  RFOOT_NOSE       = { id: 19 , p: RFOOT    , q: NOSE   , type: BIND_STICK  },
+  SHOULDER_LFOOT   = { id: 20 , p: SHOULDER , q: LFOOT  , type: REPEL_STICK },
+  SHOULDER_RFOOT   = { id: 21 , p: SHOULDER , q: RFOOT  , type: REPEL_STICK },
+  CONSTRAINTS = [
+    PEG_TAIL, TAIL_NOSE, NOSE_STRING, STRING_PEG, PEG_NOSE, STRING_TAIL,
+    PEG_BUTT, TAIL_BUTT, NOSE_BUTT,
+    SHOULDER_BUTT, SHOULDER_LHAND, SHOULDER_RHAND, BUTT_LFOOT, BUTT_RFOOT, SHOULDER_RHAND_2,
+    SHOULDER_PEG, STRING_LHAND, STRING_RHAND, LFOOT_NOSE, RFOOT_NOSE,
+    SHOULDER_LFOOT, SHOULDER_RFOOT
+  ],
+  // joints
+  STRING_PEG_TAIL = { id: 0 , s: PEG_TAIL      , t: STRING_PEG },
+  BODY_SLED =       { id: 1 , s: SHOULDER_BUTT , t: STRING_PEG },
+  JOINTS = [ STRING_PEG_TAIL, BODY_SLED ],
+  // body parts
+  BODY_PARTS = [
+    { name: 'sled'     , p: PEG      , q: STRING   },
+    { name: 'body'     , p: BUTT     , q: SHOULDER },
+    { name: 'leftArm'  , p: SHOULDER , q: LHAND    },
+    { name: 'rightArm' , p: SHOULDER , q: RHAND    },
+    { name: 'leftLeg'  , p: BUTT     , q: LFOOT    },
+    { name: 'rightLeg' , p: BUTT     , q: RFOOT    }
+  ],
+  // scarf
+  SCARF = {
+    airFriction: 0.85,
+    p: SHOULDER,
+    numSegments: 7,
+    segmentLength: 2
+  };
+
+
+function makePoints(self) {
+  self.points = POINTS.map( p => {
+    let point = new Point(p.id, p.xInit, p.yInit, p.friction);
+    return point;
+  });
+}
+
+function makeConstraints(self) {
+  self.constraints = CONSTRAINTS.map( c => {
+    let stick;
+    let p = self.points[c.p.id];
+    let q = self.points[c.q.id];
+    switch(c.type) {
+      case STICK:
+        stick = new Stick(c.id, p, q);
+        break;
+      case BIND_STICK:
+        stick = new BindStick(c.id, p, q, ENDURANCE);
+        break;
+      case REPEL_STICK:
+        stick = new RepelStick(c.id, p, q);
+        stick.restLength *= 0.5;
+        break;
+      default:
+        throw new Error('Unknown constraint type');
+    }
+    return stick;
+  });
+}
+
+function makeScarf(self) {
+  self.scarfPoints = [];
+  self.scarfConstraints = [];
+
+  for (let i = 0; i < SCARF.numSegments; i++) {
+    let p = (i === 0) ? self.points[SCARF.p.id] : self.scarfPoints[i-1];
+    let q = new Point(-i - 1, p.x - SCARF.segmentLength, p.y, 0, SCARF.airFriction);
+    self.scarfPoints.push(q);
+    self.scarfConstraints.push(new ScarfStick(-i - 1, p, q));
+  }
+}
+
+function makeJoints(self) {
+  self.joints = JOINTS.map( j =>
+    new ClockwiseCrashJoint(j.id, self.constraints[j.s.id], self.constraints[j.t.id])
+  );
+}
+
+function makeRider(self) {
+  makePoints(self);
+  makeConstraints(self);
+  makeScarf(self);
+  makeJoints(self);
+}
+
+function copyRider(self) {
+  let copy = {};
+  copy.points = _.map(self.points, point => point.clone());
+  copy.constraints = _.map(self.constraints, (c, i) => {
+    let cProps = CONSTRAINTS[i];
+    return c.clone(copy.points[cProps.p.id], copy.points[cProps.q.id]);
+  });
+
+  copy.scarfPoints = _.map(self.scarfPoints, point => point.clone());
+  copy.scarfConstraints = _.map(self.scarfConstraints, (c, i) => {
+    let p = (i === 0) ? copy.points[SCARF.p.id] : copy.scarfPoints[i-1];
+    let q = copy.scarfPoints[i];
+    return c.clone(p, q);
+  });
+
+  copy.joints = _.map(self.joints, (j, i) => {
+    let jProps = JOINTS[i];
+    j.clone(copy.constraints[jProps.s.id], copy.constraints[jProps.t.id]);
+  });
+
+  copy.crashed = self.crashed;
+  copy.sledBroken = self.sledBroken;
+  return copy;
+}
+
+
+function getBodyParts(self) {
+  let getPosition = (p, q) => {
+    let vec = q.pos.clone().subtract(p.pos);
+    return {
+      x: p.x,
+      y: p.y,
+      angle: Math.atan2(vec.y, vec.x)
+    };
+  };
+
+  let bodyParts = {};
+  BODY_PARTS.forEach(bodyPart => {
+    let p = self.points[bodyPart.p.id];
+    let q = self.points[bodyPart.q.id];
+    bodyParts[bodyPart.name] = getPosition(p, q);
+  });
+
+  bodyParts.scarf = [];
+  for (let i = 0; i < self.scarfConstraints.length; i++) {
+    let p = (i === 0) ? self.points[SCARF.p.id] : self.scarfPoints[i-1];
+    let q = self.scarfPoints[i];
+    bodyParts.scarf.push(getPosition(p, q));
+  }
+  return bodyParts;
+}
+
+module.exports = {
+  makeRider,
+  copyRider,
+  getBodyParts,
+  STRING_PEG_TAIL
+};
