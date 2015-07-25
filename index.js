d540e09e10c1f95482624a9fdcec0ecae1eed832
David Lu <davidelu93@gmail.com>
2015-07-25 18:31:32 -0400

    index.js cleanup

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-var LineTypes = require('./src/lines').LineTypes;
-
-var savedLinesReader = require('./src/saved-lines-reader');
-
-var Track = require('./src/tracks').Track;
-var OldTrack = require('./src/tracks').OldTrack;
-
-
 module.exports = {
-  Track: Track,
-  OldTrack: OldTrack,
-  LineTypes: LineTypes,
-  savedLinesReader: savedLinesReader
+  Track: require('./src/tracks').Track,
+  OldTrack: require('./src/tracks').OldTrack,
+  LineTypes: require('./src/lines').LineTypes,
+  savedLinesReader: require('./src/saved-lines-reader')
 };
