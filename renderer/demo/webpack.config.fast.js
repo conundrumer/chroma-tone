4105139f7427a82cfe16f3d89bd1f83ffcceaff5
David Lu <davidelu93@gmail.com>
2015-08-02 11:55:13 -0400

    make production config for speed

--- renderer/demo/webpack.config.fast.js
+++ renderer/demo/webpack.config.fast.js
@@ -47,7 +47,10 @@ module.exports = {
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    })
   ]
 
 };
