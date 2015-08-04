4164ff2fc42e68aae5c1ab083e2d085f68b33f2e
David Lu <davidelu93@gmail.com>
2015-08-03 21:36:35 -0400

    refactor webpack.config.fast.js

--- renderer/demo/webpack.config.fast.js
+++ renderer/demo/webpack.config.fast.js
@@ -5,52 +5,12 @@
  * the subfolder /webpack-dev-server/ is visited. Visiting the root will not automatically reload.
  */
 'use strict';
+
 var webpack = require('webpack');
 
-module.exports = {
+var config = require('./webpack.config');
+config.plugins.push(new webpack.DefinePlugin({
+  'process.env.NODE_ENV': '"production"'
+}));
 
-  output: {
-    filename: 'main.js',
-    publicPath: 'assets/'
-  },
-
-  cache: true,
-  debug: true,
-  devtool: false,
-  entry: [
-      'webpack/hot/only-dev-server',
-      './renderer/demo/main.js'
-  ],
-
-  stats: {
-    colors: true,
-    reasons: true
-  },
-
-  resolve: {
-    extensions: ['', '.js']
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'react-hot!babel-loader?stage=1'
-    }, {
-      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&minetype=image/svg+xml"
-    }, {
-      test: /\.less/,
-      loader: 'style-loader!css-loader!autoprefixer-loader!less-loader'
-    }
-    ]
-  },
-
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    })
-  ]
-
-};
+module.exports = config;
