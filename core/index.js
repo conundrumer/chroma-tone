5b5af5efb3ca11c315c2acbc4be51f3b4551bcbf
David Lu <davidelu93@gmail.com>
2015-08-16 19:30:35 -0400

    move getBoundingBox and getRiderCam

--- core/index.js
+++ core/index.js
@@ -1,7 +1,5 @@
 module.exports = {
   Track: require('./tracks').Track,
   OldTrack: require('./tracks').OldTrack,
-  LineTypes: require('./lines').LineTypes,
-  getRiderCam: require('./getRiderCam'),
-  getBoundingBox: require('./getBoundingBox')
+  LineTypes: require('./lines').LineTypes
 };
