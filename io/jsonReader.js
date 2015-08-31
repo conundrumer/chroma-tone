202ad7c458b3deac5c530205e37c8b38586735c7
David Lu <davidelu93@gmail.com>
2015-08-31 03:29:59 -0400

    implement linesArray encoding to save ~%50 space

--- io/jsonReader.js
+++ io/jsonReader.js
@@ -1,4 +1,17 @@
+const linesArrayAttr = ['type', 'id', 'x1', 'y1', 'x2', 'y2', 'extended', 'flipped', 'leftLine', 'rightLine']
+
 export default function jsonReader(json) {
+  let data = JSON.parse(json)
+  if (data.linesArray instanceof Array) {
+    data.lines = data.linesArray.map(lineData => {
+      let line = {}
+      linesArrayAttr.forEach((attr, i) => {
+        line[attr] = lineData[i]
+      })
+      return line
+    })
+    delete data.linesArray
+  }
   // TODO: don't put in array
-  return [JSON.parse(json)]
+  return [data]
 }
--- io/jsonWriter.js
+++ io/jsonWriter.js
@@ -1,3 +1,39 @@
+function isSolid(type) {
+  return type === 0 || type === 1;
+}
+
+function makeLinesArray(lines) {
+  return lines.map(({type, id, x1, y1, x2, y2, extended, flipped, leftLine, rightLine}) => {
+    let data = [type, id, x1, y1, x2, y2]
+    if (isSolid(type)) {
+      data = data.concat([extended, flipped | 0])
+      if (leftLine) {
+        data[8] = leftLine
+      }
+      if (rightLine) {
+        data[8] = typeof data[8] === 'number' ? data[8] : null
+        data[9] = rightLine
+      }
+    }
+    return data
+  })
+}
+
+function toString(trackData, spaces = 1) {
+  return JSON.stringify(trackData, null, spaces).replace(/^ +/gm, '')
+}
+
 export default function jsonWriter(trackData) {
-  return JSON.stringify(trackData, null, 1).replace(/^ +/gm, '')
+  let lines = trackData.lines
+  let json = toString(trackData)
+  if (json.length > 500000) { // 500 kb
+    let linesArray = makeLinesArray(lines)
+    delete trackData.lines
+    trackData.linesArray = linesArray
+    json = toString(trackData)
+    console.log(json.length)
+  }
+  // if (json.length > 500000) { // 500 kb
+  // }
+  return json
 }
