5a49df50d0697f723f0e58f352459a237ae1bc2f
David Lu <davidelu93@gmail.com>
2015-09-05 03:01:43 -0400

    add build script + add git-directory-deploy

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -1,15 +1,7 @@
-/*
- * Webpack distribution configuration
- *
- * This file is set up for serving the distribution version. It will be compiled to dist/ by default
- */
-
-'use strict';
-
 var webpack = require('webpack');
+var join = require('path').join
 
 module.exports = {
-
   output: {
     publicPath: 'assets/',
     path: 'dist/assets/',
@@ -18,46 +10,58 @@ module.exports = {
 
   debug: false,
   devtool: false,
-  entry: './src/components/main.js',
+  entry: './editor/main.js',
 
   stats: {
     colors: true,
     reasons: false
   },
 
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin()
-  ],
-
   resolve: {
     extensions: ['', '.js'],
     alias: {
-      'styles': __dirname + '/src/styles',
-      'mixins': __dirname + '/src/mixins',
-      'components': __dirname + '/src/components/',
-      'stores': __dirname + '/src/stores/',
-      'actions': __dirname + '/src/actions/'
+      'icons': 'material-ui-mdi/icons',
+      'assets': join(__dirname, 'assets'),
+      'core': join(__dirname, 'core'),
+      'editor': join(__dirname, 'editor'),
+      'io': join(__dirname, 'io'),
+      'renderer': join(__dirname, 'renderer')
     }
   },
-
   module: {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
+      loader: 'react-hot!babel-loader?stage=1'
     }, {
       test: /\.less/,
-      loader: 'style-loader!css-loader!less-loader'
+      loader: 'style-loader!css-loader!autoprefixer-loader!less-loader'
     }, {
-      test: /\.(png|jpg|woff|woff2)$/,
+      test: /\.css$/,
+      loader: 'style-loader!css-loader!autoprefixer-loader'
+    }, {
+      test: /\.(png|jpg)$/,
       loader: 'url-loader?limit=8192'
-    }
-]
-  }
+    }, {
+      test: /\.(woff)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=application/font-woff"
+    }, {
+      test: /\.(woff2)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=application/font-woff2"
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=image/svg+xml"
+    }]
+  },
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    })
+  ]
+
 };
