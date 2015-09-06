003c9487a298a939bbf666271e1627e6f5016d27
deploy.sh <davidelu93@gmail.com>
2015-09-06 18:26:19 -0400

    add temp track mutation logging

--- editor/main.js
+++ editor/main.js
@@ -22,6 +22,26 @@ let middlewares = [thunk]
 if (process.env.NODE_ENV !== 'production') {
   let options = { collapsed: true }
   middlewares.push(require('redux-logger')(options))
+
+  let {ADD_LINE, REMOVE_LINE, REPLACE_LINE} = require('./actions')
+  middlewares.push((store) => (next) => (action) => {
+    let {trackData: {track, lineStore}} = store.getState()
+    switch (action.type) {
+      case ADD_LINE:
+      case REMOVE_LINE:
+      case REPLACE_LINE:
+        console.log('before', track, lineStore)
+    }
+    let result = next(action)
+    ;({trackData: {track, lineStore}} = store.getState())
+    switch (action.type) {
+      case ADD_LINE:
+      case REMOVE_LINE:
+      case REPLACE_LINE:
+        console.log('after', track, lineStore)
+    }
+    return result
+  })
 }
 
 const reducer = combineReducers(reducers);
