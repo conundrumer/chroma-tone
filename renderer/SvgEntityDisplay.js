d60a005381f506378a6e1c429d7bde848bda3138
David Lu <davidelu93@gmail.com>
2015-09-12 16:50:33 -0400

    replace onecolor with chroma-js

--- renderer/SvgEntityDisplay.js
+++ renderer/SvgEntityDisplay.js
@@ -1,24 +1,28 @@
 import React from 'react'
-import color from 'onecolor'
+import chroma from 'chroma-js'
 
 class Ball extends React.Component {
   render() {
     return (
-      <circle cx={this.props.p.x} cy={this.props.p.y} r={this.props.r} fill='white' strokeWidth={1} stroke='black' />
+      <circle cx={this.props.p.x} cy={this.props.p.y} r={this.props.r * (this.props.size || 1)} fill={this.props.color || 'white'} strokeWidth={1} stroke='black' />
     );
   }
 }
 
+function getColorFromLine(p, q) {
+  let length = p.distance(q)
+  return chroma.hcl(360 * (Math.log2(length) % 1), 80, 80)
+}
+
 class Wire extends React.Component {
   render() {
-    let length = this.props.p.distance(this.props.q)
     return (
       <line
         x1={this.props.p.x}
         y1={this.props.p.y}
         x2={this.props.q.x}
         y2={this.props.q.y}
-        stroke={color(`hsv(${360 * (Math.log2(length) % 1)}, 100%, 90%)`).hex()}
+        stroke={getColorFromLine(this.props.p, this.props.q)}
         strokeWidth={2 * this.props.r}
         strokeLinecap='round'
       />
@@ -34,6 +38,11 @@ export default class SvgEntityDisplay extends React.Component {
           this.props.wires.map(wire =>
             <Wire key={wire.id} {...wire} />
           )
+        }
+        {
+          this.props.collisions.map(({entities: [ball, wire]}) => {
+            return <Ball key={-ball.id} {...ball} color={getColorFromLine(wire.p, wire.q)} size={2}/>
+          })
         }
         {
           this.props.balls.map(ball =>
