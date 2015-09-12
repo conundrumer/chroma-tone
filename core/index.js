1fb0376af4daf9f70874b5c5a8b42f19ba5c2c4b
David Lu <davidelu93@gmail.com>
2015-09-12 00:01:19 -0400

    rename entityGroup to simState

--- core/index.js
+++ core/index.js
@@ -1,15 +1,15 @@
 import vec from './vector'
 import ball from './ball'
 import wire from './wire'
-import entityGroup from './entityGroup'
+import simState from './simState'
 import collision, {CollisionType} from './collision'
 import step from './step'
 
 console.log(vec(1, 2))
 console.log(ball(vec(0, 1), vec(2, 3)))
 console.log(wire(vec(0, 0), vec(1, 1)))
-console.log(entityGroup([ball(vec(0, 1)), ball(vec(2, 3))], [wire(vec(0, 0), vec(0, 1)), wire(vec(1, 0), vec(1, 1))]))
+console.log(simState([ball(vec(0, 1)), ball(vec(2, 3))], [wire(vec(0, 0), vec(0, 1)), wire(vec(1, 0), vec(1, 1))]))
 console.log(collision(CollisionType.BALL_BALL, [ball(vec(0, 1)), ball(vec(2, 3))], 0.4))
 console.log(collision(CollisionType.BALL_WIRE, [ball(vec(0, 1)), wire(vec(0, 0), vec(0, 1))], 1.3, 0.3))
 
-export {vec, ball, wire, entityGroup, collision, CollisionType, step}
+export {vec, ball, wire, simState, collision, CollisionType, step}
--- core/simState.js
+++ core/simState.js
@@ -1,5 +1,5 @@
 // this could also return a spatial cache but too complicated
-export default function entityGroup(balls, wires) {
+export default function simState(balls, wires) {
   return {
     balls,
     wires,
