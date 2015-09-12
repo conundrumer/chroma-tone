41ade9255130e3eae02cc20838641814c0900456
David Lu <davidelu93@gmail.com>
2015-09-12 03:05:33 -0400

    add id to Ball and Wire

--- core/Ball.js
+++ core/Ball.js
@@ -5,8 +5,9 @@ const DEFAULT_BOUNCINESS = 1
 const DEFAULT_MASS = 1
 
 // balls have bounciness 1 and mass 1
-export default function Ball(pInit, vInit = Vec2(0, 0)) {
+export default function Ball(id, pInit, vInit = Vec2(0, 0)) {
   return {
+    id,
     p: pInit,
     v: vInit,
     r: DEFAULT_RADIUS,
--- core/Wire.js
+++ core/Wire.js
@@ -1,8 +1,9 @@
 const DEFAULT_RADIUS = 2
 const DEFAULT_TENSION = 1 // higher tension => higher pitch + more brightness + more bounce
 
-export default function Wire(p, q) {
+export default function Wire(id, p, q) {
   return {
+    id,
     p,
     q,
     r: DEFAULT_RADIUS,
