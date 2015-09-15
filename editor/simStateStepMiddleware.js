783f385502570df8e486ed979db421ef53bbf270
David Lu <davidelu93@gmail.com>
2015-09-15 03:01:26 -0400

    unlock web audio for ios

--- editor/simStateStepMiddleware.js
+++ editor/simStateStepMiddleware.js
@@ -63,6 +63,23 @@ function equalLength(wire1, wire2) {
 export default function simStateStep() {
   var CONTEXT = new window.AudioContext()
 
+  // https://paulbakaus.com/tutorials/html5/web-audio-on-ios/
+  function unlockWebAudio() {
+
+    // create empty buffer
+    var buffer = CONTEXT.createBuffer(1, 1, 22050);
+    var source = CONTEXT.createBufferSource();
+    source.buffer = buffer;
+
+    // connect to output (your speakers)
+    source.connect(CONTEXT.destination);
+
+    // play the file
+    source.start(0);
+    window.removeEventListener('touchstart', unlockWebAudio, false);
+  }
+  window.addEventListener('touchstart', unlockWebAudio, false);
+
   let neu = neume(CONTEXT)
 
   return ({getState}) => next => action => {
